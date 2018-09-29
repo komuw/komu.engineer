be6556d295814838ad5c05c718911079401c3ab6
komuW <komuw05@gmail.com>
2018-09-29 14:34:31 +0300

    ff

---
+++ blogs/lambda-shim/lambda.py
@@ -0,0 +1,34 @@
+import json
+import subprocess
+
+
+# 1. the python program gets a request from AWS lambda.
+# 2. it serializes that request into json.
+# 3. it writes that json into stdin
+# 4. the Go program reads from stdin
+# 5. it unmarshals what it has read from stdin and acts on it.
+# 5. it creates a json marshaled response
+# 6. it writes that json response to stdout
+# 7. the python program reads that response from stdout
+# 8. it unmarshals what it read(the response)
+# 9. it sends the response back to AWS lambda.
+
+proc = subprocess.Popen(
+    ["./main"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True, bufsize=1
+)
+
+
+def handle(event, context):
+    proc.stdin.write(json.dumps({"event": event}))
+    # read event
+    line = proc.stdout.readline()
+    event = json.loads(line)
+
+    proc.stdin.close()
+    proc.stdout.close()
+    return event
+
+
+event_value = handle(event="my_event", context={"hello": "world"})
+print("event_value::")
+print(event_value)
---
+++ blogs/lambda-shim/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"runtime"
+)
+
+//  1. the python program gets a request from AWS lambda.
+//  2. it serializes that request into json.
+//  3. it writes that json into stdin
+//  4. the Go program reads from stdin
+//  5. it unmarshals what it has read from stdin and acts on it.
+//  5. it creates a json marshaled response
+//  6. it writes that json response to stdout
+//  7. the python program reads that response from stdout
+//  8. it unmarshals what it read(the response)
+//  9. it sends the response back to AWS lambda.
+
+type ErrResponse struct {
+	Error string
+}
+
+type Request struct {
+	Event   string `json:"event,omitempty"`
+	Context string `json:"context,omitempty"`
+}
+
+type Response struct {
+	EchoEvent string `json:"echoevent,omitempty"`
+	GOOS      string `json:"goos,omitempty"`
+	GOARCH    string `json:"goarch,omitempty"`
+}
+
+func main() {
+	req := Request{}
+	err := json.NewDecoder(os.Stdin).Decode(&req)
+	if err != nil {
+		errResponse := ErrResponse{Error: "unable to json Marshal"}
+		jsonErr, _ := json.Marshal(errResponse)
+		fmt.Print(string(jsonErr))
+	}
+
+	res := Response{EchoEvent: req.Event, GOOS: runtime.GOOS, GOARCH: runtime.GOARCH}
+	b, err := json.Marshal(res)
+	if err != nil {
+		errResponse := ErrResponse{Error: "unable to json Marshal"}
+		jsonErr, _ := json.Marshal(errResponse)
+		fmt.Print(string(jsonErr))
+	}
+	fmt.Print(string(b))
+}
