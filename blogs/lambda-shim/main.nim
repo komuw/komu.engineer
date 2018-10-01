f658ef349fb83cc036180415f196baf9d3be9bef
komuW <komuw05@gmail.com>
2018-10-01 12:52:57 +0300

    add nim example

--- blogs/lambda-shim/main.nim
+++ blogs/lambda-shim/main.nim
@@ -1,8 +1,12 @@
 import json
 import times
+import  system
 
 # run this as:
 # echo '{"event": "myLambdaEventName", "context": "myLambdaContext"}'| nim compile --run main.nim
+
+# to compile to use in AWS lambda; use a 64bit linux machine:
+# nim c -d:release main.nim
 
 # read from stdin
 var request: string = readLine(stdin)
@@ -14,6 +18,7 @@ let event = jsonReq["event"]
 var response =  %*
   {
   "EchoEvent": event,
+  "Message": "hello fom Nim version: " & system.NimVersion,
   "CurrentTime":  format(times.now(), "d MMMM yyyy HH:mm")
 
   }
