0039584db6ba690d0bb6760faeb6356aa4ea1154
komuw <komuw05@gmail.com>
2018-10-08 15:57:59 +0300

    add zig example

--- blogs/lambda-shim/main.zig
+++ blogs/lambda-shim/main.zig
@@ -4,6 +4,7 @@ const std = @import("std");
 const io = std.io;
 const warn = std.debug.warn;
 const json = std.json;
+const fmt = @import("std").fmt;
 
 // run this program as:
 // echo '{"event": "myLambdaEventName", "context": "myLambdaContext"}' | /usr/local/zig/zig run main.zig
@@ -28,15 +29,26 @@ pub fn main() !void {
     var event = root.Object.get("event").?.value;
     warn("event: {}", event.String);
 
-//     var response =  %*
-//   {
-//   "EchoEvent": event,
-//   "Message": "hello fom Nim version: " & system.NimVersion,
-//   "CurrentTime":  format(times.now(), "d MMMM yyyy HH:mm")
+    const s1 =
+          \\{
+        ;
+    const s2 =
+          \\"EchoEvent": 
+        ;
+    const s3 =
+          \\}
+        ;
 
-//   }
+    var required_buf_length = 1 + s1.len + s2.len + s3.len + event.String.len;
+    warn("required_buf_length: {}", required_buf_length);
 
 
+    var all_together: [100]u8 = undefined;
+    // You can use slice syntax on an array to convert an array into a slice.
+    const all_together_slice = all_together[0..];
+    // String concatenation example.
+    const response = try fmt.bufPrint(all_together_slice, "{} {} \"{}\" {}", s1, s2, event.String, s3 );
+    warn("\n\n response {}", response );
 }
 
 // gdb ./main -ex run
