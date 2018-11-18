fcb98091e202a0686030557fba541ce0063857e4
komuW <komuw05@gmail.com>
2018-11-18 15:22:56 +0300

    update

--- blogs/timeScaleDB/code/log_collector.py
+++ blogs/timeScaleDB/code/log_collector.py
@@ -20,25 +20,27 @@ fifo_file = "/tmp/namedPipes/komusNamedPipe"
 
 
 class PIPE:
-    pipe = None
+    fifo_file = None
 
     def __enter__(self):
-        pipe = os.open(fifo_file, os.O_RDONLY | os.O_NONBLOCK | os.O_ASYNC)
-        self.pipe = pipe
-        return self.pipe
+        self.fifo_file = open(fifo_file, mode="r")
+        os.set_blocking(self.fifo_file.fileno(), False)
+
+        return self.fifo_file
 
     def __exit__(self, type, value, traceback):
-        if self.pipe:
-            os.close(self.pipe)
+        if self.fifo_file:
+            self.fifo_file.close()
 
     async def __aenter__(self):
-        pipe = os.open(fifo_file, os.O_RDONLY | os.O_NONBLOCK | os.O_ASYNC)
-        self.pipe = pipe
-        return await asyncio.sleep(-1, result=self.pipe)
+        self.fifo_file = open(fifo_file, mode="r")
+        os.set_blocking(self.fifo_file.fileno(), False)
+
+        return await asyncio.sleep(-1, result=self.fifo_file)
 
     async def __aexit__(self, exc_type, exc, tb):
-        if self.pipe:
-            await asyncio.sleep(-1, result=os.close(self.pipe))
+        if self.fifo_file:
+            await asyncio.sleep(-1, result=self.fifo_file.close())
 
 
 async def collect_logs():
@@ -46,27 +48,21 @@ async def collect_logs():
         while True:
             try:
                 logger.info("{}".format({"event": "log_collector_read"}))
-
-                # TODO figure out how to read exactly oneline
-                read_at_most = 4096  # with 4096 we are trying to read more than is sent
-
-                # TODO: we should readline
-                # TODO: try and port/add a readline implementation
-                data = os.read(pipe, read_at_most)
+                data = pipe.readline()
                 if len(data) == 0:
                     # End of the file
                     await asyncio.sleep(1)
                     continue
 
                 logger.info("{}".format({"event": "log_collector_print_data", "data": data}))
-                data = data.decode()
-                log_events = data.split("\n")
-                logs = await handle_logs(log_events=log_events)
+                log = await handle_logs(log_event=data)
 
-                # TODO: disable locks/batched log sending if we get
-                # 'got Future attached to a different loop' errors
-                async with batchedLogs.lock:
-                    batchedLogs.batch_logs = batchedLogs.batch_logs + logs
+                # do not buffer if there are no logs
+                if log:
+                    # TODO: disable locks/batched log sending if we get
+                    # 'got Future attached to a different loop' errors
+                    async with batchedLogs.lock:
+                        batchedLogs.batch_logs.append(log)
             except OSError as e:
                 if e.errno == 6:
                     pass
@@ -75,18 +71,13 @@ async def collect_logs():
                     pass
 
 
-async def handle_logs(log_events):
-    # note: we will loose some logs since not all
-    # are valid json because collect_logs() does a read of at most 4096 bytes
-    # instead of reading one line
-    logs = []
-    for i in log_events:
-        try:
-            log_event = json.loads(i)
-            logs.append(log_event)
-        except json.decoder.JSONDecodeError:
-            pass
-    return logs
+async def handle_logs(log_event):
+    log = None
+    try:
+        log = json.loads(log_event)
+    except json.decoder.JSONDecodeError:
+        pass
+    return log
 
 
 tasks = asyncio.gather(collect_logs(), schedule_log_sending(), loop=loop)
