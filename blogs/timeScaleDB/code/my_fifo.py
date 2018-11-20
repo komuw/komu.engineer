99202ad9098c6fb74961061b5eddf7938704e831
komuW <komuw05@gmail.com>
2018-11-20 18:52:24 +0300

    update

--- blogs/timeScaleDB/code/my_fifo.py
+++ blogs/timeScaleDB/code/my_fifo.py
@@ -4,6 +4,10 @@ import errno
 
 def makeFifo(fifo_directory="/tmp/namedPipes", fifo_file="komusNamedPipe"):
     fifo_file = os.path.join(fifo_directory, fifo_file)
+    if os.path.exists(fifo_file):
+        # make it indempotent to call it multiple times
+        return fifo_file
+
     try:
         os.mkdir(fifo_directory, mode=0o777)
     except OSError as e:
