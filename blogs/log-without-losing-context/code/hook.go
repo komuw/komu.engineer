85168c11f56a22e3ca7ebc844769e85dc0afdd9b
komuW <komuw05@gmail.com>
2020-05-21 15:50:37 +0300

    fix tests

--- blogs/log-without-losing-context/code/hook.go
+++ blogs/log-without-losing-context/code/hook.go
@@ -6,16 +6,18 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// TODO: docs
+// hook to buffer logs and only send at right severity.
 type hook struct {
 	writer io.Writer
 
 	// Note: in production, lineBuffer should use a circular buffer instead of a slice.
+	// otherwise you may have unbounded memory growth.
 	// we are just using a slice of []bytes here for brevity and blogging purposes.
 	lineBuffer [][]byte
 }
 
-// TODO: docs
+// Fire will append all logs to a circular buffer and only 'flush'
+// them when a log of sufficient severity(ERROR) is emitted.
 func (h *hook) Fire(entry *logrus.Entry) error {
 	line, err1 := entry.Bytes()
 	if err1 != nil {
@@ -35,7 +37,7 @@ func (h *hook) Fire(entry *logrus.Entry) error {
 	return nil
 }
 
-// TODO: docs
+// Levels define on which log levels this hook would trigger
 func (h *hook) Levels() []logrus.Level {
 	return logrus.AllLevels
 }
