cdaec3adefe29f5bc55ac2c47c9208a1f3f96873
$MY_NAME <komuw05@gmail.com>
2022-05-03 21:14:43 +0300

    f

--- blogs/10/code/main.go
+++ blogs/10/code/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -26,77 +25,48 @@ func (h httpLogger) Write(p []byte) (n int, err error) {
 		IdleConnTimeout:    10 * time.Second,
 		DisableCompression: true,
 	}
-	maxOfThreeRedirects := func(req *http.Request, via []*http.Request) error {
-		max := 3
-		if req.Header.Get("Auth") != "" {
-			// requests that have set Auth header can follow more redirects.
-			max = 12
-		}
-		if len(via) >= max {
-			return fmt.Errorf("stopped after %d redirects", max)
-		}
-		return nil
-	}
 	client := &http.Client{
-		Transport:     tr,
-		Timeout:       10 * time.Second,
-		CheckRedirect: maxOfThreeRedirects,
+		Transport: tr,
+		Timeout:   10 * time.Second,
 	}
+	// assume httpbin.org is an actual logging service.
 	resp, err := client.Post("https://httpbin.org/post", "application/json", bytes.NewReader(p))
 	return int(resp.Request.ContentLength), err
 }
 
 func main() {
-	err := logg(httpLogger{}, "hey-httpLogger")
+	err := logg(&betterhttpLogger{}, "hey-httpLogger")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-//
-// type betterhttpLogger struct {
-// 	test struct {
-// 		enabled bool
-// 		written string
-// 	}
-// }
+// betterhttpLogger logs messages to a HTTP logging service
+type betterhttpLogger struct {
+	test struct {
+		enabled bool
+		written string
+	}
+}
 
-// func (h *betterhttpLogger) Write(p []byte) (n int, err error) {
-// 	tr := &http.Transport{
-// 		MaxIdleConns:       10,
-// 		IdleConnTimeout:    10 * time.Second,
-// 		DisableCompression: true,
-// 	}
-// 	maxOfThreeRedirects := func(req *http.Request, via []*http.Request) error {
-// 		max := 3
-// 		if req.Header.Get("Auth") != "" {
-// 			// requests that have set Auth header can follow more redirects.
-// 			max = 12
-// 		}
-// 		if len(via) >= max {
-// 			return fmt.Errorf("stopped after %d redirects", max)
-// 		}
-// 		return nil
-// 	}
-// 	client := &http.Client{
-// 		Transport:     tr,
-// 		Timeout:       10 * time.Second,
-// 		CheckRedirect: maxOfThreeRedirects,
-// 	}
-// 	if h.test.enabled {
-// 		mockWriter := &bytes.Buffer{}
-// 		n, err := mockWriter.Write(p)
-// 		h.test.written = mockWriter.String()
-// 		return n, err
-// 	}
-// 	resp, err := client.Post("https://httpbin.org/post", "application/json", bytes.NewReader(p))
-// 	return int(resp.Request.ContentLength), err
-// }
-
-// In this version of the code, the only piece of code that is not tested ins just one line;
-//    `client.Post("https://httpbin.org/post", "application/json", bytes.NewReader(p))`
-// And guess what? Since it is calling code from the standard library, you can bet[https://github.com/golang/go/blob/go1.18.1/src/net/http/client_test.go#L134-L142] that it is one of
-// the most[https://github.com/golang/go/blob/go1.18.1/src/net/http/client_test.go#L606-L621] heavily tested functionality out there. So you don't have to re-test that which the Go team has/is already testing.
-//
-// If you have one interface implementation that you use in your 'real' application and another implementation that you use in tests; you are probably doing it wrong.
-// You should, in my opinion, use just one interface implementation for both application and tests. The implementation should behave a bit different conditionally on whether you are running tests or not.
+// Write fulfills io.Writer interface
+func (h *betterhttpLogger) Write(p []byte) (n int, err error) {
+	tr := &http.Transport{
+		MaxIdleConns:       10,
+		IdleConnTimeout:    10 * time.Second,
+		DisableCompression: true,
+	}
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   10 * time.Second,
+	}
+	if h.test.enabled {
+		mockWriter := &bytes.Buffer{}
+		n, err := mockWriter.Write(p)
+		h.test.written = mockWriter.String()
+		return n, err
+	}
+	// assume httpbin.org is an actual logging service.
+	resp, err := client.Post("https://httpbin.org/post", "application/json", bytes.NewReader(p))
+	return int(resp.Request.ContentLength), err
+}
--- blogs/10/code/main_test.go
+++ blogs/10/code/main_test.go
@@ -22,22 +22,22 @@ func Test_logg(t *testing.T) {
 		}
 	})
 
-	// t.Run("betterhttpLogger", func(t *testing.T) {
-	// 	msg := "hey"
-	// 	w := &betterhttpLogger{test: struct {
-	// 		enabled bool
-	// 		written string
-	// 	}{enabled: true}}
-	// 	err := logg(w, msg)
-	// 	if err != nil {
-	// 		t.Fatalf("logg() got error = %v, wantErr = %v", err, nil)
-	// 		return
-	// 	}
+	t.Run("betterhttpLogger", func(t *testing.T) {
+		msg := "hey"
+		w := &betterhttpLogger{test: struct {
+			enabled bool
+			written string
+		}{enabled: true}}
+		err := logg(w, msg)
+		if err != nil {
+			t.Fatalf("logg() got error = %v, wantErr = %v", err, nil)
+			return
+		}
 
-	// 	gotMsg := w.test.written
-	// 	wantMsg := msg + "\n"
-	// 	if gotMsg != wantMsg {
-	// 		t.Fatalf("logg() got = %v, want = %v", gotMsg, wantMsg)
-	// 	}
-	// })
+		gotMsg := w.test.written
+		wantMsg := msg + "\n"
+		if gotMsg != wantMsg {
+			t.Fatalf("logg() got = %v, want = %v", gotMsg, wantMsg)
+		}
+	})
 }
