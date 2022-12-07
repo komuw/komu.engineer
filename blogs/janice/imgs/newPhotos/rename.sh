eabc154ae4b5420ab1841fd5abf3820f35dc88f7
$MY_NAME <komuw05@gmail.com>
2022-12-07 16:05:34 +0300

    new material

--- blogs/janice/imgs/newPhotos/rename.sh
+++ blogs/janice/imgs/newPhotos/rename.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-a=1
-for i in *.jpg; do
-  new=$(printf "%02d.jpg" "$a") #02 pad to length of 2
-  mv -i -- "$i" "$new"
-  let a=a+1
-done
+
+rename_random(){
+    for i in *.jpg; do
+        uu=$(uuidgen)
+        new="${uu}.jpg"
+        mv -i -- "$i" "$new"
+    done
+}
+rename_random
+
+rename_sequential(){
+    a=1
+    for i in *.jpg; do
+        new=$(printf "%02d.jpg" "$a") #02 pad to length of 2
+        mv -i -- "$i" "$new"
+        let a=a+1
+    done
+}
+rename_sequential
