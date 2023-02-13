abceef49caba2ee5322920a0a15a65d80b670830
$MY_NAME <komuw05@gmail.com>
2023-02-13 18:37:14 +0300

    minimal service is working

---
+++ blogs/11/code/go.mod
@@ -0,0 +1,3 @@
+module opetelemetry-and-go
+
+go 1.20
---
+++ blogs/11/code/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	ctx := context.Background()
+	go serviceA(ctx, 8081)
+	serviceB(ctx, 8082)
+}
+
+// curl -vkL http://127.0.0.1:8081/serviceA
+func serviceA(ctx context.Context, port int) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/serviceA", serviceA_HttpHandler)
+	serverPort := fmt.Sprintf(":%d", port)
+	server := &http.Server{Addr: serverPort, Handler: mux}
+
+	fmt.Println("serviceA listening on", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
+
+func serviceA_HttpHandler(w http.ResponseWriter, r *http.Request) {
+	cli := &http.Client{}
+	req, err := http.NewRequestWithContext(r.Context(), http.MethodGet, "http://localhost:8082/serviceB", nil)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := cli.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Add("SVC-RESPONSE", resp.Header.Get("SVC-RESPONSE"))
+}
+
+func serviceB(ctx context.Context, port int) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/serviceB", serviceB_HttpHandler)
+	serverPort := fmt.Sprintf(":%d", port)
+	server := &http.Server{Addr: serverPort, Handler: mux}
+
+	fmt.Println("serviceB listening on", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
+
+func serviceB_HttpHandler(w http.ResponseWriter, r *http.Request) {
+	answer := add(r.Context(), 42, 1813)
+	w.Header().Add("SVC-RESPONSE", fmt.Sprint(answer))
+	fmt.Fprintf(w, "hello from serviceB: Answer is: %d", answer)
+}
+
+func add(ctx context.Context, x, y int64) int64 { return x + y }
