1406d34d308c28f377c19e1c7f9fe93afcb7b659
komuw <komuw05@gmail.com>
2024-10-18 20:15:07 +0300

    g

--- main_test.go
+++ main_test.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"fmt"
 	"io"
+	"math"
+	"math/rand/v2"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/komuw/ong/config"
 	"github.com/komuw/ong/id"
@@ -23,10 +29,27 @@ func TestMux(t *testing.T) {
 
 	w := os.Stderr
 	l := log.New(context.Background(), w, 10)
-	// mx := getMux(log.New(context.Background(), w, 10), cwd)
 	opts := config.DevOpts(l, id.New())
-	opts.Domain = "example.com"
-	mx := getMux2(l, opts, cwd)
+	opts.Domain = "localhost"
+	mx := getMux(l, opts, cwd)
+
+	httpsPort := getPort()
+	ts, err := TlsServer(mx, opts.Domain, httpsPort)
+	attest.Ok(t, err)
+	t.Cleanup(func() {
+		// It is important that we close in `t.Cleanup` rather than `defer ts.Close()`
+		// This is because using defer will close it for this fumc.
+		// When we call the server in `t.Run` it will already be closed.
+		ts.Close()
+	})
+
+	attest.Ok(t, Ping(httpsPort)) // wait for server to start.
+
+	tr := &http.Transport{
+		// since we are using self-signed certificates, we need to skip verification.
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
 
 	tests := []struct {
 		path               string
@@ -80,103 +103,169 @@ func TestMux(t *testing.T) {
 		t.Run(strings.ReplaceAll(tt.path, "/", ""), func(t *testing.T) {
 			t.Parallel()
 
-			rec := httptest.NewRecorder()
-			req := httptest.NewRequest(http.MethodGet, tt.path, nil)
-			mx.ServeHTTP(rec, req)
-
-			res := rec.Result()
+			url := ts.URL + tt.path
+			url = strings.ReplaceAll(url, "127.0.0.1", "localhost")
+			res, err := client.Get(url)
+			attest.Ok(t, err)
 			defer res.Body.Close()
 
-			attest.Equal(t, res.StatusCode, tt.expectedStatusCode)
-		})
-	}
-}
-
-func TestMuxRedirects(t *testing.T) {
-	t.Parallel()
-
-	cwd, err := os.Getwd()
-	attest.Ok(t, err)
-
-	w := os.Stderr
-	mx := getMux(log.New(context.Background(), w, 10), cwd)
-
-	t.Run("redirects", func(t *testing.T) {
-		t.Parallel()
-
-		for _, v := range []string{
-			"/blogs/go-gc-maps",
-			"/blogs/consensus",
-			"/blogs/python-lambda",
-			"/blogs/go-modules-early-peek",
-			"/blogs/lambda-shim/lambda-shim",
-			"/blogs/timeScaleDB/timescaleDB-for-logs",
-			"/blogs/celery-clone/understand-how-celery-works",
-			"/blogs/golang-stackTrace/golang-stackTrace",
-			"/blogs/log-without-losing-context/log-without-losing-context",
-		} {
-			rec := httptest.NewRecorder()
-			req := httptest.NewRequest(http.MethodGet, v, nil)
-			mx.ServeHTTP(rec, req)
-
-			res := rec.Result()
-			defer res.Body.Close()
-			attest.Equal(t, res.StatusCode, http.StatusMovedPermanently)
-		}
-	})
-}
-
-func TestMuxRouteSubdomains(t *testing.T) {
-	t.Parallel()
-
-	cwd, err := os.Getwd()
-	attest.Ok(t, err)
-
-	w := os.Stderr
-	mx := getMux(log.New(context.Background(), w, 10), cwd)
-
-	tests := []struct {
-		host               string
-		expectedStatusCode int
-		expectedBody       string
-	}{
-		{
-			host:               "localhost:80",
-			expectedStatusCode: http.StatusOK,
-			expectedBody:       "Is a software developer currently",
-		},
-		{
-			host:               "srs.komu.engineer:80",
-			expectedStatusCode: http.StatusOK,
-			expectedBody:       "this is the srs subdomain",
-		},
-		{
-			host:               "algo.komu.engineer:80",
-			expectedStatusCode: http.StatusOK,
-			expectedBody:       "4_stack_n_queue",
-		},
-	}
-
-	for _, tt := range tests {
-		tt := tt
-
-		t.Run(tt.host, func(t *testing.T) {
-			t.Parallel()
-
-			rec := httptest.NewRecorder()
-			req := httptest.NewRequest(http.MethodGet, "/index.html", nil)
-			req.Header.Set("Host", tt.host)
-			req.Host = tt.host
-
-			mx.ServeHTTP(rec, req)
-
-			res := rec.Result()
-			defer res.Body.Close()
 			rb, err := io.ReadAll(res.Body)
 			attest.Ok(t, err)
+			_ = rb
 
 			attest.Equal(t, res.StatusCode, tt.expectedStatusCode)
-			attest.Subsequence(t, string(rb), tt.expectedBody)
+			// attest.Equal(t, string(rb), msg)
+
+			// rec := httptest.NewRecorder()
+			// req := httptest.NewRequest(http.MethodGet, tt.path, nil)
+			// mx.ServeHTTP(rec, req)
+			// res := rec.Result()
+			// defer res.Body.Close()
+			// attest.Equal(t, res.StatusCode, tt.expectedStatusCode)
 		})
 	}
+}
+
+// func TestMuxRedirects(t *testing.T) {
+// 	t.Parallel()
+
+// 	cwd, err := os.Getwd()
+// 	attest.Ok(t, err)
+
+// 	w := os.Stderr
+// 	mx := getMux(log.New(context.Background(), w, 10), cwd)
+
+// 	t.Run("redirects", func(t *testing.T) {
+// 		t.Parallel()
+
+// 		for _, v := range []string{
+// 			"/blogs/go-gc-maps",
+// 			"/blogs/consensus",
+// 			"/blogs/python-lambda",
+// 			"/blogs/go-modules-early-peek",
+// 			"/blogs/lambda-shim/lambda-shim",
+// 			"/blogs/timeScaleDB/timescaleDB-for-logs",
+// 			"/blogs/celery-clone/understand-how-celery-works",
+// 			"/blogs/golang-stackTrace/golang-stackTrace",
+// 			"/blogs/log-without-losing-context/log-without-losing-context",
+// 		} {
+// 			rec := httptest.NewRecorder()
+// 			req := httptest.NewRequest(http.MethodGet, v, nil)
+// 			mx.ServeHTTP(rec, req)
+
+// 			res := rec.Result()
+// 			defer res.Body.Close()
+// 			attest.Equal(t, res.StatusCode, http.StatusMovedPermanently)
+// 		}
+// 	})
+// }
+
+// func TestMuxRouteSubdomains(t *testing.T) {
+// 	t.Parallel()
+
+// 	cwd, err := os.Getwd()
+// 	attest.Ok(t, err)
+
+// 	w := os.Stderr
+// 	mx := getMux(log.New(context.Background(), w, 10), cwd)
+
+// 	tests := []struct {
+// 		host               string
+// 		expectedStatusCode int
+// 		expectedBody       string
+// 	}{
+// 		{
+// 			host:               "localhost:80",
+// 			expectedStatusCode: http.StatusOK,
+// 			expectedBody:       "Is a software developer currently",
+// 		},
+// 		{
+// 			host:               "srs.komu.engineer:80",
+// 			expectedStatusCode: http.StatusOK,
+// 			expectedBody:       "this is the srs subdomain",
+// 		},
+// 		{
+// 			host:               "algo.komu.engineer:80",
+// 			expectedStatusCode: http.StatusOK,
+// 			expectedBody:       "4_stack_n_queue",
+// 		},
+// 	}
+
+// 	for _, tt := range tests {
+// 		tt := tt
+
+// 		t.Run(tt.host, func(t *testing.T) {
+// 			t.Parallel()
+
+// 			rec := httptest.NewRecorder()
+// 			req := httptest.NewRequest(http.MethodGet, "/index.html", nil)
+// 			req.Header.Set("Host", tt.host)
+// 			req.Host = tt.host
+
+// 			mx.ServeHTTP(rec, req)
+
+// 			res := rec.Result()
+// 			defer res.Body.Close()
+// 			rb, err := io.ReadAll(res.Body)
+// 			attest.Ok(t, err)
+
+// 			attest.Equal(t, res.StatusCode, tt.expectedStatusCode)
+// 			attest.Subsequence(t, string(rb), tt.expectedBody)
+// 		})
+// 	}
+// }
+
+func getPort() uint16 {
+	r := rand.IntN(10_000) + 1
+	p := math.MaxUint16 - uint16(r)
+	return p
+}
+
+// Taken from https://github.com/komuw/ong/blob/v0.1.11/internal/tst/tst.go#L17-L19
+func TlsServer(h http.Handler, domain string, httpsPort uint16) (*httptest.Server, error) {
+	if !testing.Testing() {
+		panic("this func should only be called from tests")
+	}
+
+	ts := httptest.NewUnstartedServer(h)
+	if err := ts.Listener.Close(); err != nil {
+		return nil, err
+	}
+
+	l, err := net.Listen("tcp", net.JoinHostPort(domain, fmt.Sprintf("%d", httpsPort)))
+	if err != nil {
+		return nil, err
+	}
+
+	ts.Listener = l
+	ts.StartTLS()
+
+	return ts, nil
+}
+
+// Taken from https://github.com/komuw/ong/blob/v0.1.11/internal/tst/tst.go#L83-L84
+func Ping(port uint16) error {
+	if !testing.Testing() {
+		panic("this func should only be called from tests")
+	}
+
+	var err error
+	count := 0
+	maxCount := 12
+
+	for {
+		count = count + 1
+		time.Sleep(1 * time.Second)
+		if _, err := net.DialTimeout("tcp", fmt.Sprintf("localhost:%d", port), 1*time.Second); err == nil {
+			fmt.Println("connected to ", port, "after: ", count)
+			break
+		}
+
+		if count > maxCount {
+			err = fmt.Errorf("ping max count(%d) reached: %w", maxCount, err)
+			break
+		}
+	}
+
+	return err
 }
